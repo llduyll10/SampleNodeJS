29fb12c2ae54c9fea9987f542c82799e6bb6d4b0
duynnd <>
2021-04-19 12:09:07 +0700

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var fs = require('fs')
 
 var cors = require('cors')
 
-app.use(cors)
+app.use(cors())
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
